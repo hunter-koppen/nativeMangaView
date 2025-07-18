998180198e45f2480b8d203a375ae6ed1a66f8db
Hunter Koppen <hunter.koppen@kobeon.nl>
2025-07-18 23:33:04 +0200

    updated to new library

--- src/components/MangaViewer.jsx
+++ src/components/MangaViewer.jsx
@@ -1,17 +1,14 @@
 import { Component, createElement, createRef } from "react";
-import { Dimensions, ScrollView, View, Image } from "react-native";
-import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
+import { Dimensions, ScrollView, View } from "react-native";
+import PhotoView from "react-native-photo-view-next";
 import { Big } from "big.js";
 
 export class MangaViewer extends Component {
     state = {
-        window: Dimensions.get("window"),
-        zoomedStates: {},
-        isScrollViewReady: false
+        window: Dimensions.get("window")
     };
     dimensionListener = null;
     scrollViewRef = createRef();
-    zoomableViewRefs = new Map();
     lastScrollUpdate = Date.now();
 
     componentDidMount() {
@@ -32,36 +29,6 @@ export class MangaViewer extends Component {
         });
     };
 
-    handleDoubleTap = itemId => {
-        const { zoomedStates } = this.state;
-        const isZoomedIn = zoomedStates[itemId] || false;
-        const zoomableViewRef = this.zoomableViewRefs.get(itemId);
-        if (zoomableViewRef?.current) {
-            if (isZoomedIn) {
-                zoomableViewRef.current.zoomTo(1);
-            } else {
-                zoomableViewRef.current.zoomTo(1.5);
-            }
-            this.setState(prevState => ({
-                zoomedStates: {
-                    ...prevState.zoomedStates,
-                    [itemId]: !isZoomedIn
-                }
-            }));
-        } else {
-            console.error("ZoomableViewRef is not defined or current is null for item:", itemId);
-        }
-    };
-
-    handleZoomChange = (zoomLevel, itemId) => {
-        this.setState(prevState => ({
-            zoomedStates: {
-                ...prevState.zoomedStates,
-                [itemId]: zoomLevel > 1
-            }
-        }));
-    };
-
     handleScroll = event => {
         const currentTime = Date.now();
         if (currentTime - this.lastScrollUpdate > 500) {
@@ -77,14 +44,8 @@ export class MangaViewer extends Component {
     handleScrollViewLayout = () => {
         const { restoreScrollLocation, scrollPosition } = this.props;
 
-        if (
-            restoreScrollLocation &&
-            !this.state.isScrollViewReady &&
-            scrollPosition.value &&
-            this.scrollViewRef?.current
-        ) {
+        if (restoreScrollLocation && scrollPosition.value && this.scrollViewRef?.current) {
             this.scrollViewRef.current.scrollTo({ y: parseInt(scrollPosition.value, 10), animated: false });
-            this.setState({ isScrollViewReady: true });
         }
     };
 
@@ -108,6 +69,7 @@ export class MangaViewer extends Component {
                 onScroll={this.handleScroll}
                 onLayout={this.handleScrollViewLayout}
                 scrollEventThrottle={64}
+                nestedScrollEnabled={true}
             >
                 <View>{topContent}</View>
                 {datasource.items?.map(item => {
@@ -116,35 +78,18 @@ export class MangaViewer extends Component {
                     const height = imageHeight.get(item).value;
                     const newHeight = this.calculateImageHeight(width, height);
 
-                    if (!this.zoomableViewRefs.has(item.id)) {
-                        this.zoomableViewRefs.set(item.id, createRef());
-                    }
-                    const zoomableViewRef = this.zoomableViewRefs.get(item.id);
-
                     return (
-                        <ReactNativeZoomableView
+                        <PhotoView
                             key={item.id}
-                            ref={zoomableViewRef}
-                            maxZoom={30}
-                            minZoom={1}
-                            initialZoom={1}
-                            bindToBorders={true}
-                            panBoundaryPadding={0}
-                            disablePanOnInitialZoom={true}
-                            onDoubleTapAfter={() => this.handleDoubleTap(item.id)}
-                            onZoomAfter={(event, gestureState, zoomableViewEventObject) =>
-                                this.handleZoomChange(zoomableViewEventObject.zoomLevel, item.id)
-                            }
-                        >
-                            <Image
-                                source={{ uri }}
-                                style={{
-                                    width: "100%",
-                                    height: newHeight,
-                                    resizeMode: "cover"
-                                }}
-                            />
-                        </ReactNativeZoomableView>
+                            source={{ uri }}
+                            minimumZoomScale={1}
+                            maximumZoomScale={3}
+                            androidScaleType="center"
+                            style={{
+                                width: window.width,
+                                height: newHeight
+                            }}
+                        />
                     );
                 })}
                 <View>{bottomContent}</View>
